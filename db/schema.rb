d830d61575b3441ae75db03200dbaef7f658e464
Ronan Lopes <lopesronanufsj@gmail.com>
2018-07-14 16:47:39 -0300

    Setup devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_193034) do
+ActiveRecord::Schema.define(version: 2018_07_14_194605) do
 
   create_table "choices", force: :cascade do |t|
     t.string "content"
@@ -61,6 +61,18 @@ ActiveRecord::Schema.define(version: 2018_07_14_193034) do
     t.integer "role_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
