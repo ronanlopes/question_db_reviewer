b1975eaf94c5e2ca30a840300686974eb5002fb1
Ronan Lopes <lopesronanufsj@gmail.com>
2018-07-14 18:29:49 -0300

    Signup user tests and feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :role
+  before_validation :set_role
+
+  validates :name, presence: true
+
+
+  def set_role
+  	self.role = Role.find_or_create_by(name: "Client") if !self.role
+  end
 
 end
---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.feature "Signup users" do
+
+	scenario "with valid credentials" do
+		visit "/"
+		click_link "Sign up"
+		fill_in "Name", with: "User Example"
+		fill_in "Email", with: "user@example.com"
+		fill_in "Password", with: "password"
+		fill_in "Password confirmation", with: "password"
+		click_button "Sign up"
+
+		expect(page).to have_content("You have signed up successfully")
+	end
+
+	scenario "with invalid credentials" do
+		visit "/"
+		click_link "Sign up"
+		fill_in "Name", with: ""
+		fill_in "Email", with: ""
+		fill_in "Password", with: ""
+		fill_in "Password confirmation", with: ""
+		click_button "Sign up"
+
+		expect(page).to have_content("Name can't be blank")
+		expect(page).to have_content("Email can't be blank")
+		expect(page).to have_content("Password can't be blank")
+
+	end
+
+
+end
