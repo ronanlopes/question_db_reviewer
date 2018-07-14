c4d889750c5fdd12e81729cc74b47ff2db8042ee
Ronan Lopes <lopesronanufsj@gmail.com>
2018-07-14 19:11:08 -0300

    sign in test

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@
 ["Administrator", "Client"].each do |role|
 	Role.find_or_create_by(name: role)
 end
+
+#Can't use find_or_create_by directly 'cause there's no "password" column
+user = User.find_by(name: "Administrator", email: "admin@email.com", role: Role.find_by_name("Administrator"))
+if !user
+	user = User.new(name: "Administrator", email: "admin@email.com", role: Role.find_by_name("Administrator"))
+	user.password = "admin123"
+	user.save
+end
---
+++ spec/features/signing_users_in_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Users signin" do
+
+	before do
+		@john = User.create!(name: "John", email: "john@example.com", password: "password")
+	end
+
+	scenario "with valid credentials" do
+
+		visit "/"
+
+		fill_in "Email", with: @john.email
+		fill_in "Password", with: @john.password
+		click_button "Log in"
+
+		expect(page).to have_content("Signed in successfully")
+		expect(page).to have_content("Welcome, #{@john.name}")
+		expect(page).not_to have_link("Sign in")
+		expect(page).not_to have_link("Sign up")
+		expect(page).to have_link("Sign out")
+
+	end
+
+
+end
