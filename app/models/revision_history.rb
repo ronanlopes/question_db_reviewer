38510e4226129f0fd7e7d96eb2490725f0779478
Ronan Lopes <lopesronanufsj@gmail.com>
2018-07-16 22:45:17 -0300

    Improve UX on forms - show details about the question and review history

--- app/models/revision_history.rb
+++ app/models/revision_history.rb
@@ -5,6 +5,9 @@ class RevisionHistory < ApplicationRecord
 
   validate :check_denied_comment
 
+  delegate :name, to: :user, prefix: :user
+  delegate :name, to: :question_status, prefix: :question_status
+
   def check_denied_comment
   	if self.question_status == QuestionStatus.find_by(name: "Denied") && !self.comment.present?
   		self.errors.add :comment, I18n.t("errors.messages.comment_required")
